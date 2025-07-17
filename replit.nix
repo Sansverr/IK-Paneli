67ab2f92576fa1a56927b22df26115e3c27c3f37
Sansverr <sanver.turt@gmail.com>
2025-07-17 08:06:56 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.wget
-    pkgs.git-lfs
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.openpyxl
+    pkgs.python311Packages.pandas
+    pkgs.python311Packages.plotly
+    pkgs.python311Packages.werkzeug
+    pkgs.python311Packages.fpdf2
   ];
 }
